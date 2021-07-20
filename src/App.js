599ffa75ee09254c8d5173cdf6006b7f52d035cb
Joao Jesus <eng.joaojeus@gmail.com>
2021-07-19 22:53:26 -0300

    used if and else to change the photo and phrase

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import './App.css'
 
 class App extends Component{
 
-  constructor(props){
+    constructor(props){
     super(props);
     this.state = {
+      imagem: imagemBiscoito,
       textoFrase:'',
       botaoClicado: false,
     };  
@@ -19,23 +20,41 @@ class App extends Component{
     'Realize o óbvio, pense no improvável e conquiste o impossível.',
     'Acredite em milagres, mas não dependa deles.',
     'A maior barreira para o sucesso é o medo do fracasso.'];
+    this.quebraBiscoito= this.quebraBiscoito.bind(this);
   }
 
   quebraBiscoito(){
     let state = this.state;
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
-    state.textoFrase = '" '+this.frases[numeroAleatorio] +' "';
+    if(this.state.botaoClicado){
+      state.textoFrase = '';   
+      state.imagem = imagemBiscoito;
+
+
+    }else{
+      let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+      state.textoFrase = '" '+this.frases[numeroAleatorio] +' "';
+      state.imagem = imagemBiscoitoQuebrado;
+    }
     this.setState(state);
-    this.setState({botaoClicado: !this.state.botaoClicado})
+    this.setState({botaoClicado:!this.state.botaoClicado})
+
   }
 
 
 
   render(){
+    var textButton = 'Abrir Biscoito'
+    if(this.state.botaoClicado){
+      textButton = 'Outro Biscoito'
+    }else{
+      textButton = 'Abrir Biscoito'
+    }
+
     return(
       <div className="container">
-        <img src={imagemBiscoito} className="img" />
-        <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>        
+        {!this.state.botaoClicado && (<img src={imagemBiscoito} className="img"/>)}
+        {this.state.botaoClicado && (<img src={imagemBiscoitoQuebrado} className="img"/>)}
+        <Botao nome={textButton} acaoBtn={this.quebraBiscoito}/>        
         <h3 className="textoFrase">{this.state.textoFrase}</h3>
         
       </div>
