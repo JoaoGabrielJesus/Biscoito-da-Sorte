a0e139d7c2f391aa9c5c163bcf04c2e2064fbcef
Joao Jesus <eng.joaojeus@gmail.com>
2021-07-20 11:52:48 -0300

    style and flexbox

--- src/App.js
+++ src/App.js
@@ -13,13 +13,32 @@ class App extends Component{
       botaoClicado: false,
     };  
 
-    this.frases = ['Siga os bons e aprenda com eles.', 
-    'O bom-senso vale mais do que muito conhecimento.', 
-    'O riso é a menor distância entre duas pessoas.', 
-    'Deixe de lado as preocupações e seja feliz.',
-    'Realize o óbvio, pense no improvável e conquiste o impossível.',
-    'Acredite em milagres, mas não dependa deles.',
-    'A maior barreira para o sucesso é o medo do fracasso.'];
+    this.frases = ['A vida trará coisas boas se tiveres paciência',
+    `Demonstre amor e alegria em todas as oportunidades e verás que a paz 
+    nasce dentro de você`,
+    'Não compense na ira o que lhe falta na razão',
+    'Defeitos e virtudes são apenas dois lados da mesma moeda',
+    'A maior de todas as torres começa no solo',
+    'Não há que ser forte. Há que ser flexível',
+    'Gente todo dia arruma os cabelos, por que não o coração?',
+    `Há três coisas que jamais voltam; a flecha lançada, a palavra dita 
+    e a oportunidade perdida`,
+    'A juventude não é uma época da vida, é um estado de espírito',
+    `'Podemos escolher o que semear, mas somos obrigados a colher
+     o que plantamos'`,
+    `Dê toda a atenção para a formação dos teus filhos, sobretudo 
+    por exemplos de tua própria vida.`,
+    'Siga os bons e aprenda com eles',
+    'Não importa o tamanho da montanha, ela não pode tapar o sol',
+    'O bom-senso vai mais longe do que muito conhecimento',
+    'Quem quer colher rosas deve suportar os espinhos',
+    'São os nossos amigos que nos ensinam as mais valiosas lições',
+    `Uma iniciativa mal-sucedida não significa o final de tudo. Sempre 
+    existe uma nova oportunidade`,
+    'Aquele que se importa com o sentimento dos outros, não é um tolo',
+    'A adversidade é um espelho que reflete o verdadeiro eu',
+    'Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos'];
+
     this.quebraBiscoito= this.quebraBiscoito.bind(this);
   }
 
@@ -28,8 +47,6 @@ class App extends Component{
     if(this.state.botaoClicado){
       state.textoFrase = '';   
       state.imagem = imagemBiscoito;
-
-
     }else{
       let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
       state.textoFrase = '" '+this.frases[numeroAleatorio] +' "';
@@ -40,8 +57,6 @@ class App extends Component{
 
   }
 
-
-
   render(){
     var textButton = 'Abrir Biscoito'
     if(this.state.botaoClicado){
@@ -50,12 +65,18 @@ class App extends Component{
       textButton = 'Abrir Biscoito'
     }
 
-    return(
+    return(      
       <div className="container">
-        {!this.state.botaoClicado && (<img src={imagemBiscoito} className="img"/>)}
-        {this.state.botaoClicado && (<img src={imagemBiscoitoQuebrado} className="img"/>)}
-        <Botao nome={textButton} acaoBtn={this.quebraBiscoito}/>        
-        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+        
+        <div className="box"> 
+          <h1>BISCOITO DA SORTE</h1>
+          {!this.state.botaoClicado && (<img src={imagemBiscoito} className="img"/>)}
+          {this.state.botaoClicado && (<img src={imagemBiscoitoQuebrado} className="img"/>)}
+          <Botao nome={textButton} acaoBtn={this.quebraBiscoito}/>        
+          <h3 className="textoFrase">{this.state.textoFrase}</h3>
+        </div>
+        
+        
         
       </div>
     );
